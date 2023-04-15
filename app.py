da5faaa794f1d7b91f49ab764ae5b3e8e4fda6cd
babita0927 <121121436+babita0927@users.noreply.github.com>
2023-04-15 18:02:09 +0530

    update app

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def upload():
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
-    #app.run(port=5001, debug=True)
+    #serve(app, host='0.0.0.0', port=8080)
+    app.run(port=5001, debug=True)
